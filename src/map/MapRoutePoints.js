07320907a5f563c36f793c8c465acb28c7282365
Marouane ELMERROUNI <melmerrouni@norsys.fr>
2026-03-16 06:25:49 +0000

    Revert "feat: smooth route path and reduce direction arrows on map"
    This reverts commit b54374325ceb5558482003788d21ef0a3aaf9aa5.

--- src/map/MapRoutePoints.js
+++ src/map/MapRoutePoints.js
@@ -78,14 +78,9 @@ const MapRoutePoints = ({ positions, onClick, showSpeedControl }) => {
       map.addControl(control, theme.direction === 'rtl' ? 'bottom-right' : 'bottom-left');
     }
 
-    const interval = Math.max(1, Math.floor(positions.length / 20));
-    const sampled = positions.filter(
-      (_, index) => index % interval === 0 || index === positions.length - 1,
-    );
-
     map.getSource(id)?.setData({
       type: 'FeatureCollection',
-      features: sampled.map((position, index) => ({
+      features: positions.map((position, index) => ({
         type: 'Feature',
         geometry: {
           type: 'Point',
@@ -99,7 +94,6 @@ const MapRoutePoints = ({ positions, onClick, showSpeedControl }) => {
         },
       })),
     });
-
     return () => map.removeControl(control);
   }, [onMarkerClick, positions, showSpeedControl]);
 
